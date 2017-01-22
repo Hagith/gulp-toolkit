adc2db19db2a98c50c7d700104807898eb00d999
Rafal Galka <rafal.galka@modernweb.pl>
2017-01-22 17:38:29 +0100

    sass with minify

--- tasks/sass.js
+++ tasks/sass.js
@@ -1,6 +1,9 @@
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
+const gulpif = require('gulp-if');
+const cleanCss = require('gulp-clean-css');
+const size = require('gulp-size');
 const plumber = require('gulp-plumber');
 const merge = require('merge-options');
 const errorHandler = require('../lib/error-handler');
@@ -9,22 +12,26 @@ const defaults = {
   src: '',
   dest: '',
   sass: {},
+  minify: {},
   autoprefixer: {
     browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1'],
     cascade: false,
   },
 };
 
-module.exports = (config) => {
+module.exports = (config, minify = false) => {
   const opts = merge.call({ concatArrays: true }, defaults, config);
 
   return function sassTask() {
     return this.src(opts.src)
-      .pipe(sourcemaps.init())
+      .pipe(gulpif(!minify, sourcemaps.init()))
       .pipe(plumber({ errorHandler }))
       .pipe(sass(opts.sass))
       .pipe(autoprefixer(opts.autoprefixer))
-      .pipe(sourcemaps.write('.'))
-      .pipe(this.dest(opts.dest));
+      .pipe(gulpif(minify, cleanCss(opts.minify)))
+      .pipe(gulpif(!minify, sourcemaps.write('.')))
+      .pipe(this.dest(opts.dest))
+      .pipe(size({ title: 'styles' }))
+      .pipe(size({ title: 'styles', gzip: true }));
   };
 };
